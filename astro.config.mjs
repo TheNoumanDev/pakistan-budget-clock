5dd97374e2ae02a9f3cf6e80762c45b399e1443b
TheNoumanDev <76642732+TheNoumanDev@users.noreply.github.com>
2026-03-27 03:27:05 +0500

    Remove cloudflare adapter for static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'static',
   integrations: [tailwind()],
-  adapter: cloudflare(),
-  site: 'https://pakbudgetreality.com',
+  site: 'https://pakbudgetclock.com',
 });
