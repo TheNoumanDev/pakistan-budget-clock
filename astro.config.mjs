76388f5a5a083a336d9dc2115bd550598affb394
TheNoumanDev <76642732+TheNoumanDev@users.noreply.github.com>
2026-03-27 03:27:42 +0500

    Update site URL to pakistanbudgetclock.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   output: 'static',
   integrations: [tailwind()],
-  site: 'https://pakbudgetclock.com',
+  site: 'https://pakistanbudgetclock.com',
 });
